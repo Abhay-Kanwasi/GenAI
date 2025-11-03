0d177b9a40d355fb3681e1ca1c84702bac19a769
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-11-03 17:31:16 +0000

    Initialized

--- learning_langchain/Chains/Runnables/RunnablePrimitives/runnable_lambda.py
+++ learning_langchain/Chains/Runnables/RunnablePrimitives/runnable_lambda.py
@@ -1,3 +1,5 @@
 """
     RunnableLambda is a runnable primitive that allows you to apply custom Python functions with an AI pipeline. It acts as a middleware between differnt AI components, enabling preprocessing, transformation, API calls, filtering and post-processing in a LangChain workflow.
 """
+
+from langchain.schema.runnable import RunnableLambda
