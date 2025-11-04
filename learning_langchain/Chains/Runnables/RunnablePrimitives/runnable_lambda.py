d6d98797fdd70eef04d2d20f98dccfee510663c1
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2025-11-04 16:41:27 +0000

    runnable lambda code

--- learning_langchain/Chains/Runnables/RunnablePrimitives/runnable_lambda.py
+++ learning_langchain/Chains/Runnables/RunnablePrimitives/runnable_lambda.py
@@ -1,5 +1,35 @@
 """
     RunnableLambda is a runnable primitive that allows you to apply custom Python functions with an AI pipeline. It acts as a middleware between differnt AI components, enabling preprocessing, transformation, API calls, filtering and post-processing in a LangChain workflow.
+    Suppose we want to create a joke about a specific topic. So user will be giving the topic and llm will generate a joke with character count in joke.
 """
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
 
-from langchain.schema.runnable import RunnableLambda
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def word_counter(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}.',
+    input_variables=['topic'],
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parllel_chain = RunnableParallel({
+    'joke' : RunnablePassthrough(),
+    'word_count' : RunnableLambda(word_counter)
+}) # or instead of using word counter we can use lambda function `lambda x: len(x.split())`
+
+final_chain = RunnableSequence(joke_gen_chain, parllel_chain)
+result = final_chain.invoke({'topic' : 'Agentic RAG'})
+print(f'Results : {result}')
